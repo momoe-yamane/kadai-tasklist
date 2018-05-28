59d2523b2ff938dd2e44ea887861045b02def4d1
momoe.yamane <momoe.yamane@techacademy.jp>
2018-05-28 07:11:10 +0000

    kadai-tasklist

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -43,6 +43,9 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
         $task = new Task;
         $task->content = $request->content;
         $task->save();
@@ -88,9 +91,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = Task::find($id);
+         $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
+        
 
         return redirect('/');
     }

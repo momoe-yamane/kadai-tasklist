5f4414599f907638827ffd64639d8ef6c6bf25be
momoe.yamane <momoe.yamane@techacademy.jp>
2018-05-29 05:06:50 +0000

    kadai-tasklist

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,21 +4,32 @@
 
 
     
-    <h1>メッセージ新規作成ページ</h1>
+    <h1>タスク新規作成ページ</h1>
     
- 
 
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-3 col-lg-6">
+    
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
 
 
 @endsection
+ </div>
+    </div>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,19 +5,28 @@
 <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
    
-
+ <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-3 col-lg-6">
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-    
+         <div class="form-group">
     　　{!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+         {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
 
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
+    
+    </div>
+</div>
     
 @endsection

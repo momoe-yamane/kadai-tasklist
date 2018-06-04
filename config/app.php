b16790d47fd777b3430abda6937d3ec6c789f102
momoe.yamane <momoe.yamane@techacademy.jp>
2018-06-04 00:44:59 +0000

    user login

--- config/app.php
+++ config/app.php
@@ -210,5 +210,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
     ],
+    
+    'Form' => Collective\Html\FormFacade::class,
+    'Html' => Collective\Html\HtmlFacade::class,
 
 ];
